e4cf9187fddec88c505f93735fbf69a90d73886c
k1jin <ke.jin@nokia-sbell.com>
2018-10-27 15:27:45 +0800

    modfiy build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
 echo "Environment: `uname -a`"
-echo "Compiler: `$CXX --version`"
+#echo "Compiler: `$CXX --version`"
 
+if [ -d build ]
+then
+    echo "removing the build folder"
+    rm -rf build
+fi
 
-rm -r build
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
+
+echo "=================cmake================"
+cmake -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. 
+
+echo "================make all targets======"
+make
+
+echo "================run string============"
+./String
+
+
+echo "================run unit testing======"
+cd ./test
+./unittest_string
+
 #make test
-make String
-cd ..
+#make String
+#echo "returning to up folder"
+cd ../../
 #codecov -t 6cab29fe-1861-468f-903e-61e4fc96107e -X gcov
