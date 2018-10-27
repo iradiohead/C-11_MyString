ad0c3979841530d60c666da0792afb4a878f28c0
k1jin <ke.jin@nokia-sbell.com>
2018-10-27 18:21:19 +0800

    code coverage token

--- .travis/cmake/CodeCoverage.cmake
+++ .travis/cmake/CodeCoverage.cmake
@@ -121,7 +121,8 @@ ENDIF() # NOT CMAKE_BUILD_TYPE STREQUAL "Debug"
 # Optional fourth parameter is passed as arguments to _testrunner
 #   Pass them in list form, e.g.: "-j;2" for -j 2
 FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
-
+    MESSAGE( WARNING "kjin-1" )
+    
     IF(NOT LCOV_PATH)
         MESSAGE(FATAL_ERROR "lcov not found! Aborting...")
     ENDIF() # NOT LCOV_PATH
@@ -132,9 +133,10 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 
     SET(coverage_info "${CMAKE_BINARY_DIR}/${_outputname}.info")
     SET(coverage_cleaned "${coverage_info}.cleaned")
+    MESSAGE( WARNING "kjin-2"${coverage_info} )    
 
     SEPARATE_ARGUMENTS(test_command UNIX_COMMAND "${_testrunner}")
-
+    MESSAGE( WARNING "kjin-3"${LCOV_PATH} )
     # Setup target
     ADD_CUSTOM_TARGET(${_targetname}
 
@@ -162,10 +164,10 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
             )
 
     # Show info where to find the report
-    #ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
-    #	COMMAND ;
-    #	COMMENT "Open ./${_outputname}/index.html in your browser to view the coverage report."
-    #)
+    ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
+    	COMMAND ;
+    	COMMENT "Open ./${_outputname}/index.html in your browser to view the coverage report."
+    )
 
 ENDFUNCTION() # SETUP_TARGET_FOR_COVERAGE
 
